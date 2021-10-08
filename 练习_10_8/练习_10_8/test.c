9a8b9c321714312023b78e4ea4062ad8f950548b
yanghongbin <3157470865@qq.com>
2021-10-08 21:54:32 +0800

    10_8

---
+++ 练习_10_8/练习_10_8/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int str = 0;
+//	scanf("%c", &str);
+//	//Ð¡Ð´
+//	if (str <= 'z' && str >= 'a')
+//	{
+//		printf("%c\n", str - 7 - 2*(str - 'a' ));
+//	}
+//	//´óÐ´
+//	else if (str <= 'Z' && str >= 'A')
+//	{
+//		double x = str;
+//		x /= 2;
+//		int z = ((int)(x / 0.1)) % 10;
+//		if (z > 4)
+//			printf("%c\n", str + str / 2 + 1);
+//		else
+//			printf("%c\n", str + str / 2);
+//	}
+//	//Êý×Ö
+//	else
+//	{
+//		char x = (str / 10) % 10 + str;
+//		printf("%c\n", x);
+//	}
+//	return 0;
+//}
