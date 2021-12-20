1b87a314048001922c983c9a0b48e2ac91b92d11
yanghongbin <3157470865@qq.com>
2021-12-20 14:00:50 +0800

    12_20

---
+++ contact3/contact3/contact1.h
@@ -0,0 +1,70 @@
+#pragma once
+
+//库函数的引用
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+//参数定义
+#define NAMEMAX 20
+#define NUMMAX 20
+#define ADRESMAX 20
+//#define MAX 10
+#define SETTING 2
+#define BASE_ADD 5
+struct PEOINF
+{
+	char name[NAMEMAX];
+	int age;
+	char phonum[NUMMAX];
+	char adres[ADRESMAX];
+};
+
+struct CONDATA
+{
+	int control;
+	int cur_capci;
+	struct PEOINF* con;
+};
+
+enum option
+{
+	EXIT,
+	ADD,
+	SEARCH,
+	DELET,
+	CHANGE,
+	SHOW,
+	SORT,
+	SAVE,
+};
+
+enum option2
+{
+	NAME = 1,
+	PHONENUM,
+	AGE,
+	ADDRES
+};
+
+
+//函数声明
+
+void Init(struct CONDATA*);
+//添加函数
+void Add(struct CONDATA*);
+//查找函数
+void Search(const struct CONDATA*);
+//删除函数
+void Delet(struct CONDATA*);
+//更改函数
+void Change(struct CONDATA*);
+//展示函数
+void Show(const struct CONDATA*);
+//排序函数
+void Sort(struct CONDATA*);
+//保存函数
+void Save(struct CONDATA*);
+//将已保存的记录存入动态内存中
+int AddFile(struct CONDATA*);
---
+++ contact3/contact3/test.c
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "contact1.h"
+
+void menu()
+{
+	printf("********1、add                 2、search**********\n");
+	printf("********3、delet               4、change**********\n");
+	printf("********5、show                6、sort************\n");
+	printf("********7、save                0、exit  **********\n");
+}
+
+Free(struct CONDATA* ps)
+{
+	free(ps->con);
+	ps->con = NULL;
+}
+int main()
+{
+	int input = 0;
+	struct CONDATA con;
+	Init(&con);
+	do
+	{
+		menu();
+		printf("请选择:>\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case ADD:
+			Add(&con);
+			break;
+		case SEARCH:
+			Search(&con);
+			break;
+		case DELET:
+			Delet(&con);
+			break;
+		case CHANGE:
+			Change(&con);
+			break;
+		case SHOW:
+			Show(&con);
+			break;
+		case SORT:
+			Sort(&con);
+			break;
+		case SAVE:
+			Save(&con);
+			break;
+		case EXIT:
+			Save(&con);
+			Free(&con);
+			printf("已保存，退出通讯录\n······");
+			break;
+		default:
+			printf("输入错误，请重新输入。\n");
+			break;
+		}
+	} while (input);
+	return 0;
+}
